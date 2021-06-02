2083e5cbc289a97943318360473ffcc218c134e2
chris_h_ <christianevrhuetter@gmail.com>
2021-06-02 12:55:33 +0200

    adaptions for paths

--- cartoGRAPHs_app/helpers.py
+++ cartoGRAPHs_app/helpers.py
@@ -4,6 +4,6 @@ from sys import platform
 if (platform == "darwin"):
     filePre = ''
 if (platform == "linux"):
-    filePre = "/var/www/appSLC/appSLC/"
+    filePre = "/var/www/cartoGRAPHs_app/cartpGRAPHs_app/"
 if (platform == "win32"):
     filePre = ''

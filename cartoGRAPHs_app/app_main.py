8e8fd943bf62240546bf08f87ccab517ea91df08
Celine Sin <celine.sin@gmail.com>
2021-06-10 20:26:30 +0200

    IP try tbb treading on numba

--- cartoGRAPHs_app/app_main.py
+++ cartoGRAPHs_app/app_main.py
@@ -110,6 +110,17 @@ import warnings
 from numba import config, njit, threading_layer
 # set the threading layer before any parallel target compilation
 config.THREADING_LAYER = 'safe'
+
+@njit(parallel=True)
+def foo(a, b):
+    return a + b
+
+x = np.arange(10.)
+y = x.copy()
+
+# this will force the compilation of the function, select a threading layer
+# and then execute in parallel
+foo(x, y)
 # demonstrate the threading layer chosen
 print("Threading layer chosen: %s" % threading_layer())
 
